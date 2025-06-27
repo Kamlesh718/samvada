af189c969b2015c06327c3232c69f6b14382adb4
Kamlesh718 <kamleshvishwakarma1202@gmail.com>
2025-06-28 00:02:44 +0530

    params fixes

--- src/app/api/contacts/[id]/route.ts
+++ src/app/api/contacts/[id]/route.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
-  const { id } = params;
+  const { id } = context.params;
 
   await connectToDB();
 
