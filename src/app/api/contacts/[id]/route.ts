2676fc44c78093e7d7627a8b96f3ce1cc3db9813
Kamlesh718 <kamleshvishwakarma1202@gmail.com>
2025-06-28 14:25:14 +0530

    Fix GET handler type for Next.js build

--- src/app/api/contacts/[id]/route.ts
+++ src/app/api/contacts/[id]/route.ts
@@ -2,11 +2,12 @@ import { connectToDB } from "@/lib/mongoose";
 import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const { id } = params;
+interface Context {
+  params: { id: string };
+}
+
+export async function GET(req: NextRequest, context: Context) {
+  const { id } = context.params;
 
   await connectToDB();
 
